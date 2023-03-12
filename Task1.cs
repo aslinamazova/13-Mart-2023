01f9fa34a46f17b142ed17798020342f941f1f38
aslinamazova <76001yw@code.edu.az>
2023-03-13 00:43:32 +0400

    first commit

---
+++ Task1.cs
@@ -0,0 +1,8 @@
+﻿int num = 5987;
+int sum = 0;
+while (num != 0)
+{
+    sum = (sum * 10) + (num % 10);
+    num /= 10;
+}
+Console.WriteLine(sum);
---
+++ Task2.cs
@@ -0,0 +1,6 @@
+﻿int num = 257;
+string sum = "0";
+string x = "3";
+sum = x+ num;
+
+Console.WriteLine(sum);
