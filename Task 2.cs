4d0b77208f2f35d42f2bb92de898c564df902a09
aslinamazova <76001yw@code.edu.az>
2023-03-13 10:49:20 +0400

    first commit

---
+++ Task 2.cs
@@ -0,0 +1,15 @@
+﻿int result = 257;
+int num = 257;
+int count = 0;
+int sum = 0;
+while (num > 0)
+{
+    num -= num % 10;
+    num /= 10;
+    count++;
+    sum = result + (3 * (10 ** count));
+}
+Console.WriteLine(sum);
+
+
+
